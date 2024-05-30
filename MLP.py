53c92a2b5445dfc2b3390469bbabab956967a6bd
deciantis <7785171+Deciantis@users.noreply.github.com>
2024-05-29 22:02:57 -0400

    set weights & sigmoid function

---
+++ MLP.py
@@ -0,0 +1,26 @@
+import numpy as np
+import math
+
+class Perceptron:
+    """A single neuron with the sigmoid activation function.
+       Attributes:
+          inputs: The number of inputs in the perceptron, not counting the bias.
+          bias:   The bias term. By default it's 1.0."""
+
+    def __init__(self, inputs, bias = 1.0):
+        """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
+        self.weights = (np.random.rand(inputs + 1) * 2) - 1
+        self.bias = bias
+
+    def run(self, x):
+        """Run the perceptron. x is a python list with the input values."""
+        x_sum = np.dot(np.append(x,self.bias),self.weights)
+    
+    def set_weights(self, w_init):
+           self.weights = np.array(w_init)
+
+    def sigmoid(self, x):
+        sig = 1/(1 - np.exp(-x))
+        return sig
+        
+        
