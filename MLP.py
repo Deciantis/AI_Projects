05488a858e2aa9a89cca924bff4f16d86de38c7d
deciantis <7785171+Deciantis@users.noreply.github.com>
2024-05-29 23:51:59 -0400

    multi-perceptrons

--- MLP.py
+++ MLP.py
@@ -15,6 +15,7 @@ class Perceptron:
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
         x_sum = np.dot(np.append(x,self.bias),self.weights)
+        return self.sigmoid(x_sum)
     
     def set_weights(self, w_init):
            self.weights = np.array(w_init)
@@ -23,4 +24,48 @@ class Perceptron:
         sig = 1/(1 - np.exp(-x))
         return sig
         
+
+
+class MultiLayerPerceptron:     
+    """A multilayer perceptron class that uses the Perceptron class above.
+       Attributes:
+          layers:  A python list with the number of elements per layer.
+          bias:    The bias term. The same bias is used for all neurons.
+          eta:     The learning rate."""
+
+    def __init__(self, layers, bias = 1.0):
+        """Return a new MLP object with the specified parameters.""" 
+        self.layers = np.array(layers,dtype=object)
+        self.bias = bias
+        self.network = [] # The list of lists of neurons
+        self.values = []  # The list of lists of output values
+
+        for i in range(len(self.layers)):
+            self.values.append([])
+            self.network.append([])
+            self.values[i] = [0.0 for j in range(self.layers[i])]
+            if i > 0:      #network[0] is the input layer, so it has no neurons
+                for j in range(self.layers[i]): 
+                    self.network[i].append(Perceptron(inputs = self.layers[i-1], bias = self.bias))
         
+        self.network = np.array([np.array(x) for x in self.network],dtype=object)
+        self.values = np.array([np.array(x) for x in self.values],dtype=object)
+
+        # Challenge: Finish the set_weights() and run() methods:
+
+    def set_weights(self, w_init):
+        """Set the weights. 
+        w_init is a 3D list with the weights for all but the input layer."""
+        for i in range(len(w_init)):
+            for j in range(len(w_init[i])):
+                self.network[i+1][j].set_weights(w_init[i][j])       
+
+    def print_weights(self):
+        print()
+        for i in range(1,len(self.network)):
+            for j in range(self.layers[i]):
+                print("Layer",i+1,"Neuron",j,self.network[i][j].weights)
+        print()
+
+    def run(self, x):
+        return self.values[-1]
